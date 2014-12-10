e54ff196f9fb74b9d1cc409193db66016b68fad2
Jason Gullickson <jason.gullickson@gmail.com>
2014-12-10 10:01:45 -0600

    Updates for lastest Octoprint API (master branch)

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,64 +1,77 @@
-var printing = false;
-
 function fetchPrinterStatus() {
 
   var octoprint_host = localStorage.getItem('octoprinthost');
   var octoprint_port = localStorage.getItem('octoprintport');
   var octoprint_api_key = localStorage.getItem('octoprintapikey');
 
-  var octoprint_api_url = 'http://' + octoprint_host + ':' + octoprint_port + '/api/state?apikey=' + octoprint_api_key;
-  
+  var octoprint_api_url = 'http://' + octoprint_host + ':' + octoprint_port + '/api/job?apikey=' + octoprint_api_key;
+
   var response;
   var req = new XMLHttpRequest();
   req.open('GET', octoprint_api_url, true);
   req.onload = function(e) {
-  if (req.readyState == 4) {
-    if(req.status == 200) {
-      response = JSON.parse(req.responseText);
-      
-      var filename = response.job.filename;
-      
-      // send notification when done
-      if(printing && !response.state.flags.printing){
-        var d = new Date();
-        Pebble.showSimpleNotificationOnPebble('Printing Complete', filename + ' finished printing at ' + d);
+    if (req.readyState == 4) {
+      if(req.status == 200) {
+        // init default display values
+        var filename = 'no file loaded';
+        var remaining_string = '00:00';
+        var job_status = 'Connected';
+
+        // parse response
+        response = JSON.parse(req.responseText);
+
+        // only update if a job is loaded
+        if(response.job.file.name){
+          
+          // update filename
+          filename = response.job.file.name;
+
+          // update time remaining
+          var remaining_seconds = 0;
+          if(response.progress.printTimeLeft){
+            remaining_seconds = response.progress.printTimeLeft;
+          } else {
+            remaining_seconds = response.job.estimatedPrintTime;
+          }
+          
+          var remaining_hours = Math.floor((remaining_seconds / 60) / 60);
+          var remaining_minutes = Math.floor((remaining_seconds / 60) % 60);
+          remaining_string = remaining_hours + ':' + remaining_minutes;
+
+          // update job status
+          var prog_percent = Math.round(Number(response.progress.completion));
+          job_status = response.state;
+          if(prog_percent > 0){
+            job_status = job_status + ' ' + prog_percent + '%';
+          }
+          
+          // Issue: too long filenames break messaging, so
+          // for now, trimming them to 23chrs max
+          filename = filename.substring(0,20) + '...';
+        }
+        
+          // debug
+          console.log(filename);
+          console.log(remaining_string);
+          console.log(job_status);
+        
+          // update status on watch
+          Pebble.sendAppMessage({
+            "0":filename,
+            "1":remaining_string,
+            "2":job_status}, appMessageACK, appMessageNACK);
+  
+          // send notification when done
+          if(response.job.progress.completion == 100){
+            var d = new Date();
+            Pebble.showSimpleNotificationOnPebble('Printing Complete', filename + ' finished printing at ' + d);
+          }
       }
-      
-      printing = response.state.flags.printing;
-      
-      var remaining = response.progress.printTimeLeft;
-      var remaining_string = '00:00';
-      
-      if(remaining){
-        remaining_string = remaining.toString().substring(0,5);
-      }
-      
-      remaining = remaining_string;
-      
-      var prog_percent = Math.round(Number(response.progress.progress) * 100);
-      prog_percent = prog_percent + '% complete';
-      var progress = prog_percent.toString();
-      
-      console.log(filename);
-      console.log(remaining);
-      console.log(progress);
-      
-      // Issue: too long filenames break messaging, so
-      // for now, trimming them to 23chrs max
-      filename = filename.substring(0,20) + '...';
-      
-      Pebble.sendAppMessage({
-        "0":filename,
-        "1":remaining,
-        "2":progress}, appMessageACK, appMessageNACK);
-      }
-    
     } else {
-    
       console.log('something went wrong, ' + req.status);
-      
     }
-  }
+  };
+  
   req.send(null);
 }
 
